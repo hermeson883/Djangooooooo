92b44a9c6b90db214c1cc9129111aa910e113986
HERMESON DO VALE BESERRA <202108145807@alunos.estacio.br>
2023-03-13 10:17:31 -0300

    Adicionando questions e html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Questions
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    latest_question_list = Questions.objects.order_by('-public_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
